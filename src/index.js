bb0cd7db191abcf120ae29a71121e3a2f79412d2
mason <15539588217@163.com>
2025-11-27 18:04:55 +0800

    feat: base finish

--- src/index.js
+++ src/index.js
@@ -1,50 +1,44 @@
 const {
   console,
-  menu,
   event,
-  utils
+  file,
+  mpv,
 } = iina;
 
-console.log("Plugin is running:Auto LoadSub");
-// ln -s /Users/mason/my/autoloadsub ~/Library/Application\ Support/com.colliderli.iina/plugins/com.mason.autoloadsub.iinaplugin-dev
-// 插件加载
-// event.on("iina.file-loaded", (url) => {
-//   console.log('fileLoad:', url);
+const prefix = '/Users/mason/115挂载/学习/字幕';
+const suffixes = ['srt', 'ass', 'vtt', 'pgs'];
+
+event.on("iina.file-loaded", (url) => {
+  if (!url) return;
+
+  // 只处理 http/https 视频
+  if (!url.startsWith("http://") && !url.startsWith("https://")) {
+    return;
+  }
+
+  // 获取 subtitle 参数
+  const subPath = getSubtitlePath(url);
+  console.log('subPath', subPath);
   
-//   if (!url) return;
+  if (!subPath) return;
 
-//   // 只处理 http/https 视频
-//   if (!url.startsWith("http://") && !url.startsWith("https://")) {
-//     return;
-//   }
+  // 加载字幕
+  mpv.command('sub-add',[subPath]);
+});
 
-//   // 获取 subtitle 参数
-//   const subPath = getSubtitlePath(url);
-//   if (!subPath) return;
 
-//   // 加载字幕
-//   plugin.player.addSubtitles(subPath);
-// });
 
-// const prefix = '/Users/mason/115挂载/学习/字幕';
-// const suffixes = ['srt', 'ass', 'vtt', 'pgs'];
+// 从 URL 中解析 subTitle
+function getSubtitlePath(url) {
+  const name = url.split("&name=")[1];
+  if (!name) return null;
+  const decodeSubName = decodeURIComponent(name);
+  const suffix = suffixes.find(item =>
+    file.exists(`${prefix}/${decodeSubName}.${item}`)
+  );
 
-// // 从 URL 中解析 subTitle
-// function getSubtitlePath(url) {
-//   const query = url.split("?")[1];
-//   if (!query) return null;
-
-//   const params = new URLSearchParams(query);
-//   const subName = params.get("name");
-//   if (!subName) return null;
-
-//   const decodeSubName = decodeURIComponent(subName);
-//   const suffix = suffixes.find(item =>
-//     fs.existsSync(`${prefix}/${decodeSubName}.${item}`)
-//   );
-
-//   return suffix && `${prefix}/${decodeSubName}.${suffix}`;
-// }
+  return suffix && `${prefix}/${decodeSubName}.${suffix}`;
+}
 
 
 
